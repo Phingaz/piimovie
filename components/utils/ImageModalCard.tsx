504245e4699c0808de136a3c0ece5ff5a3806d44
Prosper <piinoya@gmail.com>
2025-03-21 16:26:12 -0300

    bug fixes

--- components/utils/ImageModalCard.tsx
+++ components/utils/ImageModalCard.tsx
@@ -2,10 +2,10 @@
 import React from 'react';
 import ImageComponent from './ImageComponent';
 import { imageCardUrl, imageUrl } from '@/lib/utils';
-import { Poster } from '@/app/types/movies';
 import useModal from '@/app/_hooks/useModal';
 import Modal from './Modal';
 import Image from 'next/image';
+import { Poster } from '@/app/types/utils';
 
 const ImageModalCard = ({ el }: { el: Poster }) => {
   const imageModal = useModal();
