f222e33138430866c65a834878c370b8ffa85f92
Prosper <piinoya@gmail.com>
2025-04-02 17:54:04 -0300

    included vote count

--- components/utils/buttons/FavAndReview.tsx
+++ components/utils/buttons/FavAndReview.tsx
@@ -10,7 +10,7 @@ import { ListType } from '@/app/types/utils';
 const FavAndReview = ({ type, movie, title }: { type: ListType; movie: Movie | Show; title: string }) => {
   return (
     <div className="absolute top-0 right-0 p-2 cursor-pointer z-20 w-full flex justify-between">
-      <Ratings showBg isReview vote_average={movie.vote_average} />
+      <Ratings showBg isReview vote_average={movie.vote_average} voteCount={movie?.vote_count} />
       <Favorite type={type} movie={{ ...movie, title } as unknown as movie} />
     </div>
   );
