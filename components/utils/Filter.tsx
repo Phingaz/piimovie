b015c596a5ba8119187dd1a52d9d96c2e93f9ae4
phingaz-apollo <prosper.agwegiokhe@apollographql.com>
2025-04-08 13:48:20 -0300

    Refactor FilterSection component to remove ListType dependency and adjust props for improved clarity

--- components/utils/Filter.tsx
+++ components/utils/Filter.tsx
@@ -6,7 +6,6 @@ import { Switch } from '@/components/ui/switch';
 import { SelectComponentUrl } from './Select';
 import { countries, movieGenres, releaseType, sortOptions, tvGenres } from '@/lib/arrys';
 import { cn } from '@/lib/utils';
-import { ListType } from '@/app/types/utils';
 import { useFilterName, useFilterState, useQueryParams } from '@/app/_hooks/useQueryParams';
 import { CheckBoxes, FilterTitle, MultiComboBoxes, ResetButton, Sliders } from './FilterHelpers';
 import { FilterEnum } from '@/lib/enums';
@@ -15,11 +14,11 @@ import { DatePicker } from '../ui/date-picker';
 import { Button } from '../ui/button';
 import ModalComponent from '../general/Modal';
 
-export const FilterSection = ({ className, type }: { className?: string; type: ListType }) => {
+export const FilterSection = ({ className }: { className?: string }) => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const updateQueryParams = useQueryParams();
-  const { isMovie } = useMainCtx();
+  const { type, isMovie } = useMainCtx();
 
   const includeAdult = searchParams.get(FilterEnum.INCLUDE_ADULT) === 'true';
 
